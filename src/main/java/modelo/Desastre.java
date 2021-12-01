5ea8d48dc8d4793940717ed81092e51c9b834274
Manlet Sánchez <manlet.sanchez@gmail.com>
2021-12-01 11:07:52 -0300

    Lista clase Persona:

--- src/main/java/modelo/Desastre.java
+++ src/main/java/modelo/Desastre.java
@@ -3,4 +3,7 @@ package modelo;
 public class Desastre {
 	private Epicentro epicentro;
 	private TipoDesastre estado;
+
+
+
 }
--- src/main/java/modelo/Persona.java
+++ src/main/java/modelo/Persona.java
@@ -6,4 +6,101 @@ public class Persona {
 	private int edad;
 	private String rut;
 	private EstadoPersona estado;
+	private String pais;
+	private String sexo;
+	private TipoDesastre tipoDesastre;
+
+	public Persona (String nombre, String apellido, int edad, String rut, String pais, String sexo, EstadoPersona estado){
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.edad = edad;
+		this.rut = rut;
+		this.pais = pais;
+		this.sexo = sexo;
+		this.estado = estado;
+		this.tipoDesastre = tipoDesastre;
+	}
+
+	public String getNombre(){
+		return naombre;
+	}
+
+	public void setNombre(String nombre){
+		this.nombre = nombre;
+	}
+
+	public String getApellido(){
+		return apellido;
+	}
+
+	public void setApellido(String apellido){
+		this.apellido = apellido;
+	}
+
+	public int getEdad(){
+		return edad;
+	}
+
+	public void setEdad(int edad){
+		this.edad = edad;
+	}
+
+	public String getRut(){
+		return rut;
+	}
+
+	public void setRut(String rut){
+		this.rut = rut;
+	}
+
+	public String getPais(){
+		return pais;
+	}
+
+	public void setPais(String pais){
+		this.pais = pais;
+	}
+
+	public String getSexo(){
+		return sexo;
+	}
+
+	public void setSexo(String sexo){
+		this.sexo = sexo;
+	}
+
+	public EstadoPersona getEstado(){
+		return estado;
+	}
+
+	public void setEstado(EstadoPersona estado){
+		this.estado = estado;
+	}
+
+	@Override
+	public String toString() {
+		return "Desaparecido{" +
+				"nombre = " + nombre + '\'' +
+				", apellido = " + apellido + '\'' +
+				", edad = " + edad + '\'' +
+				", rut =" + rut +'\'' +
+				", pais = " + pais +'\'' +
+				", sexo = " + sexo +'\'' +
+				", estado = " + estado +'\'' +
+				" } ";
+	}
+
+	public String[] arreglo() {
+		String nombre = this.nombre;
+		String apellido = this.apellido;
+		int edad = this.edad;
+		String rut = this.rut;
+		String pais = this.pais;
+		String sexo = this.sexo;
+		EstadoPersona estado = this.estado;
+		String nombreDesastre = this.tipoDesastre.getNombre();
+		return new String[]{nombre,apellido,edad,rut,pais,sexo,estado,tipoDesastre};
+	}
+
+	}
 }
